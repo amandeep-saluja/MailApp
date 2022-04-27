42069bc331903d05b7cbb0920e208c254d8ab4db
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-04-27 22:25:33 +0530

    made mails dynamically displayed

--- js/script.js
+++ js/script.js
@@ -21,27 +21,21 @@ function appendAllMails() {
     mailList.innerHTML = '';
 
     data.forEach(mail => {
-        let mailDiv = document.createElement('div');
-        mailDiv.classList.add('mail');
-        
-        let firstRow = document.createElement('div');
-        firstRow.classList.add('first-row');
-        
-        let title = document.createElement('p');
-        title.classList.add('title');
+        let mailDiv = createElementWithClass('div', 'mail');
+
+        let firstRow = createElementWithClass('div', 'first-row');
+
+        let title = createElementWithClass('p', 'title');
         title.textContent = mail['messageTitle'];
         firstRow.appendChild(title);
         firstRow.innerHTML += '<i class="fa fa-circle icon"></i>';
 
-        let secondRow = document.createElement('div');
-        secondRow.classList.add('second-row');
-        
-        let sentBy = document.createElement('p');
-        sentBy.classList.add('sent-by');
+        let secondRow = createElementWithClass('div', 'second-row');
+
+        let sentBy = createElementWithClass('p', 'sent-by');
         sentBy.textContent = mail['sentBy'];
 
-        let mailDate = document.createElement('p');
-        mailDate.classList.add('mail-date');
+        let mailDate = createElementWithClass('p', 'mail-date');
         mailDate.textContent = mail['messageDeliveryDate'];
 
         secondRow.appendChild(sentBy);
@@ -50,11 +44,47 @@ function appendAllMails() {
         mailDiv.appendChild(secondRow);
 
         mailList.appendChild(mailDiv);
+
+        mailDiv.addEventListener('click', () => showMail(mail));
     });
 }
 
 function showMail(mail) {
-    
+    let mailPane = document.querySelector(".mail-pane");
+    mailPane.innerHTML = "";
+
+    let mailHeader = createElementWithClass("div", "mail-header");
+
+    let mailFirstRow = createElementWithClass("div", "mail-first-row");
+    let title = createElementWithClass("p", "title");
+    title.textContent = mail['messageTitle'];
+    mailFirstRow.appendChild(title);
+    mailFirstRow.innerHTML += '<i class="fa fa-trash icon" title="Delete this mail"></i>';
+
+    let secondRow = createElementWithClass("div", "second-row");
+    let sentBy = createElementWithClass("p", "sent-by");
+    sentBy.textContent = mail['sentBy'];
+    let mailDate = createElementWithClass("p", "mail-date");
+    mailDate.textContent = mail['messageDeliveryDate'];
+
+    secondRow.appendChild(sentBy);
+    secondRow.appendChild(mailDate);
+
+    mailHeader.appendChild(mailFirstRow);
+    mailHeader.appendChild(secondRow);
+
+    mailPane.appendChild(mailHeader);
+
+    let mailBody = createElementWithClass("div", "mail-body");
+    mailBody.textContent = mail['messageBody'];
+
+    mailPane.appendChild(mailBody);
+}
+
+function createElementWithClass(elementName, className) {
+    let element = document.createElement(elementName);
+    element.classList.add(className);
+    return element;
 }
 
 init();
