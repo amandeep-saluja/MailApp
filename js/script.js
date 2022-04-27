a20c1c8045008539186858e12c5f56044de86ae0
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-04-28 00:31:22 +0530

    inbox and trash working

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-let data = [];
+let data = new Map();
 
 function init() {
     window.fetch('./data/mailData.json')
         .then(res => res.json()
             .then(resp => {
-                data = resp;
+                resp.forEach(record => data.set(record['id'], record));
+                // data = resp;
                 updateCounts();
-                appendAllMails();
+                appendAllMails('inbox');
+                document.querySelector('.inbox.box-menu').addEventListener('click', () => appendAllMails());
+                document.querySelector('.trash.box-menu').addEventListener('click', () => appendAllMails('trash'));
             }))
         .catch(err => console.log(err));
 }
 
 function updateCounts() {
     let inboxCount = document.querySelector('#inbox-count');
-    inboxCount.textContent = data.length;
+    inboxCount.textContent = [...data.values()].filter(e => e['isRead'] == false).length;
 }
 
-function appendAllMails() {
+function appendAllMails(category = 'inbox') {
     let mailList = document.querySelector('.mail-list');
     mailList.innerHTML = '';
 
-    data.forEach(mail => {
+    let allMails = [];
+
+    if (category == 'inbox') {
+        allMails = [...data.values()].filter(m => m['isDelete'] == false);
+    } else if (category == 'trash') {
+        allMails = [...data.values()].filter(m => m['isDelete'] == true);
+    }
+
+    allMails.forEach(mail => {
         let mailDiv = createElementWithClass('div', 'mail');
 
         let firstRow = createElementWithClass('div', 'first-row');
@@ -28,7 +39,8 @@ function appendAllMails() {
         let title = createElementWithClass('p', 'title');
         title.textContent = mail['messageTitle'];
         firstRow.appendChild(title);
-        firstRow.innerHTML += '<i class="fa fa-circle icon"></i>';
+        if (!mail['isRead'])
+            firstRow.innerHTML += '<i class="fa fa-circle icon"></i>';
 
         let secondRow = createElementWithClass('div', 'second-row');
 
@@ -45,11 +57,16 @@ function appendAllMails() {
 
         mailList.appendChild(mailDiv);
 
-        mailDiv.addEventListener('click', () => showMail(mail));
+        mailDiv.addEventListener('click', () => {
+            showMail(mail);
+            appendAllMails(category);
+            updateCounts();
+        });
     });
 }
 
 function showMail(mail) {
+    data.get(mail['id'])['isRead'] = true;
     let mailPane = document.querySelector(".mail-pane");
     mailPane.innerHTML = "";
 
@@ -59,7 +76,8 @@ function showMail(mail) {
     let title = createElementWithClass("p", "title");
     title.textContent = mail['messageTitle'];
     mailFirstRow.appendChild(title);
-    mailFirstRow.innerHTML += '<i class="fa fa-trash icon" title="Delete this mail"></i>';
+    if(!mail['isDelete'])
+        mailFirstRow.innerHTML += '<i class="fa fa-trash icon" title="Delete this mail"></i>';
 
     let secondRow = createElementWithClass("div", "second-row");
     let sentBy = createElementWithClass("p", "sent-by");
@@ -79,12 +97,24 @@ function showMail(mail) {
     mailBody.textContent = mail['messageBody'];
 
     mailPane.appendChild(mailBody);
+    document.querySelector(".fa.fa-trash.icon").addEventListener('click', () => {
+        data.get(mail['id'])['isDelete'] = true;
+        mailPane.innerHTML = "";
+        appendAllMails('inbox');
+        updateCounts();
+        trashCount();
+    });
 }
 
 function createElementWithClass(elementName, className) {
     let element = document.createElement(elementName);
     element.classList.add(className);
     return element;
+}
+
+function trashCount() {
+    let trashCount = document.querySelector('#trash-count');
+    trashCount.textContent = [...data.values()].filter(e => e['isDelete'] == true).length;
 }
 
 init();
